dfab274ad9a18b3d556871adc4dc4536249a9e79
Adam Majerski <adam.majerski@op.pl>
2019-11-30 23:18:21 +0100

    login

--- Project_management/mainwindow.xaml.cs
+++ Project_management/mainwindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using database_layer;
+
+
 
 namespace Project_management
 {
@@ -24,12 +27,49 @@ namespace Project_management
         {
             InitializeComponent();
         }
+        string log_t;
 
         private void OpenWindow(object sender, RoutedEventArgs e)
         {
-            SelectionMenuAdmin objSMA = new SelectionMenuAdmin();
+            /*SelectionMenuAdmin objSMA = new SelectionMenuAdmin();
             this.Visibility = Visibility.Hidden;
             objSMA.Show();
+            */
+            log_t = login_text.Text;
+            if(login_text.Text == "" || password_text.Text == "")
+            {
+                MessageBox.Show("Błędne dane logowania!","Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else
+            {
+                bool access = Login.login(login_text.Text, password_text.Text);
+                if (access)
+                {
+                    SelectionMenuAdmin objSMA = new SelectionMenuAdmin();
+                    this.Visibility = Visibility.Hidden;
+                    objSMA.Show();
+                }
+                else
+                    MessageBox.Show("Błędne dane logowania!(spróbuj admin,123)");
+
+
+            }
+        }
+    }
+
+    class Login
+    {
+      public static bool login(string log_f, string pass_f)
+        {
+            DataClasses1DataContext context = new DataClasses1DataContext();
+            var q = from t in context.login_tab
+                    where t.login == log_f && 
+                    t.password == pass_f
+                    select t;
+
+
+                return (q.Count() == 1);
+
         }
     }
 }
