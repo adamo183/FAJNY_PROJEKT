e07c71af6f0c1cc76b9c17c9df7f35bc0ad2819f
Maciej Romanek <maciejt.romanek@gmail.com>
2019-12-06 00:04:16 +0100

    Dodałem trzy okna widoczne po logowaniu i w tych oknach pracujemy wedle wcześniejszych ustaleń

---
+++ Project_management/SML_Subject.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_management
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SML_Subject.xaml
+    /// </summary>
+    public partial class SML_Subject : Window
+    {
+        public SML_Subject()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project_management/SelectionMenuLecturer.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_management
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SelectionMenuLecturer.xaml
+    /// </summary>
+    public partial class SelectionMenuLecturer : Window
+    {
+        public SelectionMenuLecturer()
+        {
+            InitializeComponent();
+        }
+
+        private void SML_CreateSubject_button(object sender, RoutedEventArgs e)
+        {
+            SML_Subject SML_Subject_window = new SML_Subject();
+            this.Visibility = Visibility.Hidden;
+            SML_Subject_window.Show();
+        }
+
+        private void SML_SectionManagement_button(object sender, RoutedEventArgs e)
+        {
+            SML_SectionManagement SML_SectionManagement_window = new SML_SectionManagement();
+            this.Visibility = Visibility.Hidden;
+            SML_SectionManagement_window.Show();
+        }
+
+        private void SML_Presence_button(object sender, RoutedEventArgs e)
+        {
+            SML_Presence SML_Presence_window = new SML_Presence();
+            this.Visibility = Visibility.Hidden;
+            SML_Presence_window.Show();
+        }
+    }
+}
