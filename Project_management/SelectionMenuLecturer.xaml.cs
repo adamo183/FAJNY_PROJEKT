653c8b4d9a8022f77d4443a924e512835559629c
Adam Majerski <adam.majerski@op.pl>
2019-12-08 21:10:13 +0100

    sync

--- Project_management/SelectionMenuLecturer.xaml.cs
+++ Project_management/SelectionMenuLecturer.xaml.cs
@@ -27,21 +27,21 @@ namespace Project_management
         private void SML_CreateSubject_button(object sender, RoutedEventArgs e)
         {
             SML_Subject SML_Subject_window = new SML_Subject();
-            this.Visibility = Visibility.Hidden;
+             //this.Visibility = Visibility.Hidden;
             SML_Subject_window.Show();
         }
 
         private void SML_SectionManagement_button(object sender, RoutedEventArgs e)
         {
             SML_SectionManagement SML_SectionManagement_window = new SML_SectionManagement();
-            this.Visibility = Visibility.Hidden;
+            //this.Visibility = Visibility.Hidden;
             SML_SectionManagement_window.Show();
         }
 
         private void SML_Presence_button(object sender, RoutedEventArgs e)
         {
             SML_Presence SML_Presence_window = new SML_Presence();
-            this.Visibility = Visibility.Hidden;
+            //this.Visibility = Visibility.Hidden;
             SML_Presence_window.Show();
         }
     }
